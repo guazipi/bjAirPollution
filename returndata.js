0f21c65b812e6b9a8dd2688e9f61a53436b89676
chetliu <sam_lhx@126.com>
2015-11-26 12:15:44 +0800

    增添样式文件和字体文件

---
+++ returndata.js
@@ -0,0 +1,207 @@
+var avc=[{
+    "date": "2015-11-25 11:00:00+09:00",
+    "samples": [{
+        "stationId": "120",
+        "coordinates": [139.885077, 35.708978],
+        "wind": [22.5, 2.4],
+        "so2": 0.001
+    }, {"stationId": "138", "coordinates": [139.877188, 35.681928], "wind": [0, 2.9], "so2": null}, {
+        "stationId": "232",
+        "coordinates": [139.43226, 35.682151],
+        "wind": [22.5, 0.5],
+        "so2": null
+    }, {
+        "stationId": "140",
+        "coordinates": [139.870805, 35.653665],
+        "wind": [22.5, 2.1],
+        "so2": null
+    }, {
+        "stationId": "146",
+        "coordinates": [139.339888, 35.64496],
+        "wind": [45, 1.5],
+        "so2": 0.001
+    }, {"stationId": "248", "coordinates": [139.781678, 35.678908], "wind": [45, 1], "so2": null}, {
+        "stationId": "101",
+        "coordinates": [139.768119, 35.692752],
+        "wind": [22.5, 0.5],
+        "so2": null
+    }, {
+        "stationId": "102",
+        "coordinates": [139.779176, 35.656316],
+        "wind": [22.5, 1.4],
+        "so2": 0.001
+    }, {"stationId": "103", "coordinates": [139.73536, 35.642334], "wind": [0, 1], "so2": null}, {
+        "stationId": "136",
+        "coordinates": [139.778513, 35.632454],
+        "wind": [292.5, 0.8],
+        "so2": 0.001
+    }, {
+        "stationId": "104",
+        "coordinates": [139.710632, 35.687139],
+        "wind": [337.5, 0.7],
+        "so2": 0
+    }, {"stationId": "105", "coordinates": [139.755093, 35.73338], "wind": [45, 2.9], "so2": null}, {
+        "stationId": "106",
+        "coordinates": [139.82767, 35.689954],
+        "wind": [0, 4.7],
+        "so2": null
+    }, {"stationId": "107", "coordinates": [139.720987, 35.610754], "wind": [0, 1.6], "so2": null}, {
+        "stationId": "213",
+        "coordinates": [139.679121, 35.623241],
+        "wind": [157.5, 0.4],
+        "so2": null
+    }, {
+        "stationId": "145",
+        "coordinates": [139.75275, 35.600814],
+        "wind": [22.5, 2.2],
+        "so2": 0.001
+    }, {
+        "stationId": "215",
+        "coordinates": [139.709849, 35.59621],
+        "wind": [292.5, 0.7],
+        "so2": null
+    }, {"stationId": "108", "coordinates": [139.682482, 35.619685], "wind": [0, 1.9], "so2": null}, {
+        "stationId": "109",
+        "coordinates": [139.739667, 35.557402],
+        "wind": [22.5, 3.3],
+        "so2": 0.002
+    }, {"stationId": "110", "coordinates": [139.6529, 35.645974], "wind": [45, 2.3], "so2": 0.001}, {
+        "stationId": "142",
+        "coordinates": [139.595416, 35.651673],
+        "wind": [45, 1.6],
+        "so2": null
+    }, {
+        "stationId": "234",
+        "coordinates": [139.613039, 35.665816],
+        "wind": [22.5, 1],
+        "so2": null
+    }, {
+        "stationId": "112",
+        "coordinates": [139.641002, 35.719229],
+        "wind": [45, 3.2],
+        "so2": null
+    }, {
+        "stationId": "113",
+        "coordinates": [139.602224, 35.690483],
+        "wind": [22.5, 2],
+        "so2": null
+    }, {
+        "stationId": "114",
+        "coordinates": [139.789178, 35.73311],
+        "wind": [45, 2.5],
+        "so2": 0.001
+    }, {
+        "stationId": "115",
+        "coordinates": [139.708153, 35.759573],
+        "wind": [22.5, 2.1],
+        "so2": null
+    }, {
+        "stationId": "243",
+        "coordinates": [139.735941, 35.766331],
+        "wind": [45, 0.3],
+        "so2": null
+    }, {"stationId": "116", "coordinates": [139.599098, 35.737739], "wind": [360, 0], "so2": null}, {
+        "stationId": "117",
+        "coordinates": [139.664243, 35.76534],
+        "wind": [45, 1.8],
+        "so2": null
+    }, {
+        "stationId": "226",
+        "coordinates": [139.804383, 35.77489],
+        "wind": [67.5, 2.2],
+        "so2": 0.001
+    }, {
+        "stationId": "137",
+        "coordinates": [139.651975, 35.743023],
+        "wind": [45, 2.2],
+        "so2": null
+    }, {
+        "stationId": "118",
+        "coordinates": [139.776423, 35.780151],
+        "wind": [67.5, 1.9],
+        "so2": 0.001
+    }, {
+        "stationId": "143",
+        "coordinates": [139.82582, 35.769878],
+        "wind": [22.5, 2.5],
+        "so2": null
+    }, {
+        "stationId": "119",
+        "coordinates": [139.877474, 35.744475],
+        "wind": [45, 2.4],
+        "so2": null
+    }, {
+        "stationId": "141",
+        "coordinates": [139.869274, 35.786401],
+        "wind": [67.5, 1],
+        "so2": null
+    }, {
+        "stationId": "148",
+        "coordinates": [139.28757, 35.627083],
+        "wind": [337.5, 0.6],
+        "so2": null
+    }, {
+        "stationId": "149",
+        "coordinates": [139.293068, 35.671391],
+        "wind": [45, 1.1],
+        "so2": null
+    }, {
+        "stationId": "122",
+        "coordinates": [139.407887, 35.714],
+        "wind": [112.5, 0.5],
+        "so2": null
+    }, {
+        "stationId": "123",
+        "coordinates": [139.556187, 35.71285],
+        "wind": [45, 2.1],
+        "so2": 0.001
+    }, {
+        "stationId": "124",
+        "coordinates": [139.275848, 35.787854],
+        "wind": [112.5, 1],
+        "so2": 0.002
+    }, {
+        "stationId": "125",
+        "coordinates": [139.478125, 35.669055],
+        "wind": [112.5, 0.5],
+        "so2": null
+    }, {"stationId": "126", "coordinates": [139.554582, 35.666414], "wind": [0, 1.3], "so2": null}, {
+        "stationId": "127",
+        "coordinates": [139.450539, 35.535725],
+        "wind": [315, 2.3],
+        "so2": 0.001
+    }, {"stationId": "144", "coordinates": [139.481654, 35.591859], "wind": [0, 1.9], "so2": null}, {
+        "stationId": "128",
+        "coordinates": [139.503048, 35.699474],
+        "wind": [45, 0.8],
+        "so2": null
+    }, {
+        "stationId": "129",
+        "coordinates": [139.476591, 35.727776],
+        "wind": [90, 2.1],
+        "so2": 0.001
+    }, {
+        "stationId": "131",
+        "coordinates": [139.326945, 35.738429],
+        "wind": [90, 1.1],
+        "so2": 0.001
+    }, {
+        "stationId": "132",
+        "coordinates": [139.570445, 35.635155],
+        "wind": [22.5, 1.2],
+        "so2": 0.001
+    }, {"stationId": "133", "coordinates": [139.425794, 35.751654], "wind": [360, 0], "so2": null}, {
+        "stationId": "134",
+        "coordinates": [139.521115, 35.779903],
+        "wind": [67.5, 1.2],
+        "so2": 0.001
+    }, {"stationId": "135", "coordinates": [139.4325, 35.634933], "wind": [0, 0.8], "so2": 0.001}, {
+        "stationId": "130",
+        "coordinates": [139.534432, 35.728033],
+        "wind": [45, 2],
+        "so2": null
+    }, {"stationId": "139", "coordinates": [139.557974, 35.749968], "wind": [67.5, 1.2], "so2": null}]
+}];
+/**
+ * Created by chet on 15/11/26.
+ */
